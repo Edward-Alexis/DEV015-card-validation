19bf5d5d8437eb468d2e5e3e2cc880e16bd25403
Edward <edwardalexisquispequispe083@gmail.com>
2024-10-03 17:28:49 -0500

    completo

--- docs/index.js
+++ docs/index.js
@@ -10,3 +10,28 @@ document.getElementById('validateBtn').addEventListener('click', function() {
     document.getElementById('validationMessage').textContent = "Tarjeta Inválida";
   }
 });
+
+document.getElementById('cardNumber').addEventListener('input', function (e) {
+  let input = e.target.value;
+  
+  input = input.replace(/\s/g, '');
+  
+  let formattedInput = input.match(/.{1,4}/g)?.join(' ') || '';
+  
+  e.target.value = formattedInput;
+});
+
+const fechaInput = document.getElementById('fecha');
+
+fechaInput.addEventListener('input', function(e) {
+  let value = e.target.value.replace(/\D/g, '');
+  
+  if (value.length >= 2) {
+    value = value.slice(0, 2) + ' / ' + value.slice(2, 4);
+  }
+  
+  e.target.value = value;
+  
+  const inputLength = value.length;
+  fechaInput.setSelectionRange(inputLength, inputLength);
+});
